a4a487df05b6784fd5d8c73ca7648922592666e9
Alicya <75876981+alicyabilqis@users.noreply.github.com>
2025-05-19 18:19:47 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -379,7 +379,7 @@ fig.update_layout(
 )
 
 fig.update_traces(
-    textposition='middle center',
+    textposition='center',
     marker=dict(line=dict(width=0)),
     textfont=dict(color='black', family='Arial Black')
 )
